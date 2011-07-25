eb96f941fd663ffcacac7d5c0fed89cdd5f71b6b
Brian C <bac2k@hotmail.com>
2011-07-24 23:16:12 -0500

    AI can now find it's next open shot, using a horizontal pattern

--- lib/Farragut.php
+++ lib/Farragut.php
@@ -1,6 +1,34 @@
 <?php
     class Farragut {
-        public function shoot($board) {
+        public function chooseShot($board) {
+            return $this->horizontalShot($board); 
+        }
+
+        /* Scans the board row by row, taking shots in every other column.
+         * The result is a horizontal striping pattern.
+         * Params: $board - a 2x2 matrix of the current state of the board 
+         * Return: On success, an array of [row, column] for the next shot.
+         * On failure, throws an exception 
+         */
+        private function horizontalShot($board) {
+            $rows = array_keys($board);
+            $startingColumn = 0;
+            $maxColumn = count($board[$rows[0]]) - 1;
+            $column = 0;
+            for ($row = current($rows); $row !== false ; $row = next($rows)) {
+                for ($column = $startingColumn; $column <= $maxColumn; $column += 2) {
+                    if ($board[$row][$column] === 0) {
+                        return array($row, $column);
+                    }
+                }
+
+                // No shots available in this row, so toggle offset
+                // and move to next one
+                $startingColumn = $startingColumn ? 0 : 1;
+            }
+
+            // Didn't find a shot at all...
+            throw new Exception('No shot found!');
         }
     }
 ?>
--- tests/Farragut.php
+++ tests/Farragut.php
@@ -1,22 +1,33 @@
 <?php
-    require_once('Farragut');
+    require_once 'lib/Farragut.php';
+
     class FarragutTest extends PHPUnit_Framework_TestCase {
 
-        public function testShoot() {
-            $fakeBoard = array('A' => array(0, 0, 0), 'B' => array(0, 0, 0));
+        /**
+         * @test
+         */
+        public function chooseShot() {
+            // Setup
+            $board = array(
+                'A' => array(0, 0, 0),
+                'B' => array(0, 0, 0)
+            );
             $farragut = new Farragut();
 
             // First shot should be top left corner
-            $shot = $farragut->shoot(fakeBoard);
+            $shot = $farragut->chooseShot($board);
             $this->assertEquals(array('A', 0), $shot);
-            $fakeBoard['A'][0] = 1;
 
-            // Successive shots should follow a diagonal
-            $shot = $farragut->shoot(fakeBoard);
+            // Successive shots should go every other column
+            $board['A'][0] = 1;
+            $shot = $farragut->chooseShot($board);
+            $this->assertEquals(array('A', 2), $shot);
+
+            // A new row should start when the edge of the board is reached
+            $board['A'][2] = 1;
+            $shot = $farragut->chooseShot($board);
             $this->assertEquals(array('B', 1), $shot);
         }
-
     }
-    
 
 ?>
