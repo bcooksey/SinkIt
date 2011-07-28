7249a923c789f6bc27ad4169f40d94384b70be7c
Brian C <bac2k@hotmail.com>
2011-07-27 22:33:39 -0500

    Adds an index page and the main page for the game

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "root/header.php"; ?>
+<body>
+    <script type="text/javascript">
+        function newGame() { window.location = 'root/game.php'; }
+    </script>
+    <div class="intro-page">
+        <span id="new-game" onclick="newGame()">Start a new game</span>
+    </div>
+</body>
+<?php include "root/footer.php"; ?>
---
+++ root/game.php
@@ -0,0 +1,5 @@
+<?php include "header.php"; ?>
+<body>
+
+</body>
+<?php include "footer.php"; ?>
